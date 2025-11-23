19ec7f8ddd11cbe809ecf0f2b5cda9121a207a4a
strangelookingnerd <49242855+strangelookingnerd@users.noreply.github.com>
2025-11-23 12:07:09 +0100

    Migrate tests to JUnit5 (#116)
    * Migrate annotations and imports
    * Migrate assertions
    * Remove public visibility for test classes and methods
    * Minor code cleanup

--- src/test/java/org/jenkinsci/plugins/variant/VariantTest.java
+++ src/test/java/org/jenkinsci/plugins/variant/VariantTest.java
@@ -4,37 +4,39 @@ import hudson.model.Action;
 import org.jenkinsci.plugins.variant.pkg.Negative5;
 import org.jenkinsci.plugins.variant.pkg.Positive5;
 import org.jenkinsci.plugins.variant.pkg2.Negative6;
-import org.junit.AfterClass;
-import org.junit.Assert;
-import org.junit.BeforeClass;
-import org.junit.Rule;
-import org.junit.Test;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.jvnet.hudson.test.Issue;
 import org.jvnet.hudson.test.JenkinsRule;
+import org.jvnet.hudson.test.junit.jupiter.WithJenkins;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 /**
  * @author Kohsuke Kawaguchi
  */
-public class VariantTest extends Assert {
-    @Rule
-    public JenkinsRule j = new JenkinsRule();
+@WithJenkins
+class VariantTest {
 
-    @BeforeClass
-    public static void setUp() {
+    @BeforeAll
+    static void setUp() {
         VariantSet.INSTANCE = new VariantSet("test");
     }
 
-    @AfterClass
-    public static void tearDown() {
+    @AfterAll
+    static void tearDown() {
         VariantSet.INSTANCE = new VariantSet();
     }
 
     @Test
-    public void test() {
-        List<Class> classes = new ArrayList<Class>();
+    void test(JenkinsRule j) {
+        List<Class<?>> classes = new ArrayList<>();
         for (Action a : j.jenkins.getActions()) {
             classes.add(a.getClass());
         }
@@ -42,25 +44,24 @@ public class VariantTest extends Assert {
         assertTrue(classes.contains(Positive2.class));
         assertTrue(classes.contains(Positive3.class));
         assertTrue(classes.contains(Positive5.class));
-        assertTrue(!classes.contains(Negative1.class));
-        assertTrue(!classes.contains(Negative2.class));
-        assertTrue(!classes.contains(Negative5.class));
-        assertTrue(!classes.contains(Negative6.class));
+	    assertFalse(classes.contains(Negative1.class));
+	    assertFalse(classes.contains(Negative2.class));
+	    assertFalse(classes.contains(Negative5.class));
+	    assertFalse(classes.contains(Negative6.class));
     }
 
     @Test
     @Issue("JENKINS-37317")
-    public void testRequiredClass() {
-        List<Class> classes = new ArrayList<Class>();
+    void testRequiredClass(JenkinsRule j) {
+        List<Class<?>> classes = new ArrayList<>();
         for (Action a : j.jenkins.getActions()) {
             classes.add(a.getClass());
         }
-        assertTrue("Negative 3 should not exist", !classes.contains(Negative3.class));
+	    assertFalse(classes.contains(Negative3.class), "Negative 3 should not exist");
 
-        for (Class klass : classes) {
+        for (Class<?> klass : classes) {
             System.out.println(klass.getCanonicalName());
-            assertTrue("Negative 4 should not exist",
-                    !klass.getCanonicalName().equals("org.jenkinsci.plugins.variant.Negative4"));
+	        assertNotEquals("org.jenkinsci.plugins.variant.Negative4", klass.getCanonicalName(), "Negative 4 should not exist");
         }
 
         assertTrue(classes.contains(Positive4.class));
